5e2e37ad2e5a3da76bbabead4ed4eb3f41e7243e
UsoD98 <130049758+UsoD98@users.noreply.github.com>
2023-12-26 08:26:13 +0900

    [level unrated] Title: 양꼬치, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int share = n / 10;
+        k -= share;
+        return answer = 12000*n + 2000*k;
+    }
+}
