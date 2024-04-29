065d2c12250967f1bba28624d45d4170a1ea8dc9
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-04-29 22:14:34 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,8 @@
+def solution(a, b, n):
+    answer = 0
+    while n >= a:
+        rest = n % a
+        answer += n // a * b
+        n = n // a * b + rest
+        print(n,answer)
+    return answer
