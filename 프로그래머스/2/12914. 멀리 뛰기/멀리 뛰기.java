22bf3e505fdb0d233f8cf42e36a40abef177907d
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-08-21 13:30:43 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int n) {
+        if(n<=2) return n;
+        
+  
+        long[] list = new long[n+1];
+        list[1] = 1;
+        list[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            list[i] = (list[i-1] + list[i-2]) % 1234567;
+        }
+
+
+        return list[n];     
+        }
+    
+}
