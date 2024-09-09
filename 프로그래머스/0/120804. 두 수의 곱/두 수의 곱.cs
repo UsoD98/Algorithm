e93711daeb948851ae2e722927c7c875b34cb39a
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-09-09 12:38:31 +0900

    [level 0] Title: 두 수의 곱, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = calcMul(num1, num2);
+        return answer;
+    }
+    
+    private int calcMul(int num1, int num2) {
+        return num1 * num2;
+    }
+}
