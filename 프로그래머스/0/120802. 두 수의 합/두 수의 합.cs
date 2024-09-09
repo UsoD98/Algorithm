de62f053bcbe0f60e34e92c45dcc736a21e89170
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-09-09 12:27:32 +0900

    [level 0] Title: 두 수의 합, Time: 1.04 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer;
+        
+        answer = addNums(num1, num2);
+        
+        return answer;
+    }
+    
+    private int addNums(int num1,int num2) {
+        return num1 + num2;
+    }
+}
